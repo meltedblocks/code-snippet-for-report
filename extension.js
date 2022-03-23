0f85afadc04818a33f54f03c3f5a3312b39b38c2
f44z <vgood@protonmail.com>
2022-03-23 14:52:28 +0100

    Add pretty ident functionality

--- extension.js
+++ extension.js
@@ -1,7 +1,62 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const { start } = require('repl');
-const vscode = require('vscode');
+const vscode = require("vscode");
+
+function isIdentChar(char) {
+  //9 - tabs
+  //32 - spaces
+  const ident_ascii = [9, 32];
+  return !!ident_ascii.includes(char.charCodeAt(0));
+}
+
+function removeIdent(text) {
+  const lines = text.split("\n");
+  let min_ident = null;
+
+  for (let key in lines) {
+    let ident = 0;
+
+    const item = lines[key];
+
+    //Ignore empty lines
+    if (item.length === 0) {
+      continue;
+    }
+
+    if (!isIdentChar(item[0])) {
+      min_ident = 0;
+      break;
+    }
+
+    for (let i in item) {
+      if (ident >= min_ident && min_ident !== null) {
+        break;
+      }
+      if (!isIdentChar(item[i])) {
+        break;
+      }
+      ident++;
+    }
+
+    if (ident < min_ident || min_ident === null) {
+      min_ident = ident;
+    }
+  }
+
+  if (min_ident === 0) {
+    return lines.join("\n");
+  }
+
+  for (let key in lines) {
+    //Ignore empty lines
+    if (lines[key].length === 0) {
+      continue;
+    }
+    lines[key] = lines[key].substring(min_ident);
+  }
+
+  return lines.join("\n");
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -10,52 +65,52 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  let disposable = vscode.commands.registerCommand(
+    "code-snippet-for-report.generateSnippet",
+    async function () {
+      try {
+        // The code you place here will be executed every time your command is executed
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+          return; // No open text editor
+        }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "code-snippet-for-report" is now active!');
+        const selection = editor.selection;
+        const text = removeIdent(editor.document.getText(selection));
+        const startLine = selection._start._line + 1;
+        const lang = editor.document.languageId;
+        const workspacePath = vscode.workspace.rootPath;
+        const fullPath = editor.document.fileName;
+        let pathToFile = fullPath;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('code-snippet-for-report.generateSnippet', async function () {
-		// The code you place here will be executed every time your command is executed
-		const editor = vscode.window.activeTextEditor;
-		if (!editor) {
-			return; // No open text editor
-		}
-	
-		const selection = editor.selection;
-		const text = editor.document.getText(selection);
-		const startLine = selection._start._line + 1;
-		const lang = editor.document.languageId;
-		const workspacePath = vscode.workspace.rootPath;
-		const fullPath = editor.document.fileName;
-		let pathToFile = fullPath;
+        if (fullPath.split(workspacePath).length > 1) {
+          pathToFile = fullPath.split(workspacePath)[1];
+        }
 
-		if (fullPath.split(workspacePath).length > 1) {
-			pathToFile = fullPath.split(workspacePath)[1];
-		}
+        if (pathToFile[0] === "/" || pathToFile[0] === "\\") {
+          pathToFile = pathToFile.substring(1);
+        }
 
-		if (pathToFile[0] === "/" || pathToFile[0] === "\\") {
-			pathToFile = pathToFile.substring(1);
-		}
-
-		const snippet = `\`\`\`{language=${lang} caption=${pathToFile} firstnumber=${startLine} hlines=${startLine}}
+        const snippet = `\`\`\`{language=${lang} caption=${pathToFile} firstnumber=${startLine} hlines=${startLine}}
 ${text}
 \`\`\`
-`
+`;
 
-		await vscode.env.clipboard.writeText(snippet);
-	});
+        await vscode.env.clipboard.writeText(snippet);
+      } catch (e) {
+        vscode.window.showInformationMessage("Some error, check console.");
+        console.error(e);
+      }
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
